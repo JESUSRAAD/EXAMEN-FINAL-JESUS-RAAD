74847d43aa0b1e169e29bf461fd1ee20f8222573
JESUSRAAD <jesus.raad@students.gammatech.school>
2024-09-19 22:58:35 +0200

    frist try drilling

---
+++ src/app/(main)/drillingInfo/ComponetGrandSon.jsx
@@ -0,0 +1,19 @@
+
+
+const ComponetGrandSon = ({x, incrementX}) => {
+
+
+    return (
+        <div className="border-[2px] border-black p-2">
+          <h2 className="text-xl">ComponentC</h2>
+          <p>x = {x}</p>
+          <button
+          className="flex justify-center items-center bg-blue-400 text-white  rounded-xl px-2 py-1" 
+          onClick={incrementX}>
+            x++
+          </button>
+        </div>
+      );
+    }
+
+export default ComponetGrandSon
