2e9546fc0ec5d81761d5e6d878d5cb3ec8370689
JESUSRAAD <jesus.raad@students.gammatech.school>
2024-09-19 22:12:31 +0200

    solutions

--- src/app/(main)/galeriaDeCartas/GallerySection.jsx
+++ src/app/(main)/galeriaDeCartas/GallerySection.jsx
@@ -40,7 +40,7 @@ const GallerySection = () => {
                   <p className="font-normal">{coffe.price.toFixed(2)} €</p>
                 </div>
       
-                <button  disabled  className="flex items-center text-center py-3 px-6 min-h-[40px] justify-center max-w-fit text-white rounded font-semibold text-sm leading-4 bg-[#2a5b45]">Añadir</button>
+                <button  disabled  className="flex justify-center items-center min-w-[60px] min-h-[32px] text-[white] rounded text-sm font-semibold leading-4 bg-[#2a5b45b2]  group-hover:bg-[#2a5b45] group-hover:cursor-pointer">Añadir</button>
               </div>
             );
           })}</div>
